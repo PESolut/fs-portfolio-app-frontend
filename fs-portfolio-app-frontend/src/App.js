fd25d801bc9bf46c1a910a69e974f2c1f6bb7efb
PESolut <jahaadpetty@pursuit.org>
2023-02-07 21:13:32 -0500

    base react app setup

---
+++ fs-portfolio-app-frontend/src/App.js
@@ -0,0 +1,11 @@
+import RouteComponent from "./Components/RouteComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <RouteComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fs-portfolio-app-frontend/src/Components/RouteComponent.js
@@ -0,0 +1,12 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+// import all pages here
+
+const RouteComponent = () => {
+    return (
+        <Routes>
+              // return routes here
+        </Routes>
+    );
+};
+
+export default RouteComponent;
