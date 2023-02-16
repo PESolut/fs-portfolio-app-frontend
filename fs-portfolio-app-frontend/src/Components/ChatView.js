4ef113ff9b625db60b712bfd3d158c20cef170c0
PESolut <jahaadpetty@pursuit.org>
2023-02-16 11:44:07 -0500

    chat page styled and reformated

--- fs-portfolio-app-frontend/src/Components/ChatView.js
+++ fs-portfolio-app-frontend/src/Components/ChatView.js
@@ -2,10 +2,19 @@ import ChatViewItem from "./ChatViewItem"
 import '../Components/Styles/ChatView.css'
 
 const ChatView = ({messages}) => {
+    console.log('ChatView localstorage isAuth state:',localStorage.getItem('isAuth'))
+    console.log('ChatView userinfo', localStorage.getItem('userData'))
+    const userString = localStorage.getItem('userData')
+    const userJson = JSON.parse(userString)
+    console.log('ChatView userinfo parsed', userJson)
+    const userName = userJson.name
+    const userEmail = userJson.email
+    const userId = userJson.id
+    console.log ('Users Info:','name:',userName,'id',userId,'email',userEmail)
 
     return (
         <>
-            <ul>
+            <ul className="chat-view-list">
                 {
                     messages.map(message => <ChatViewItem key={message.id} message={message} id={message.id} />)
                 } 
--- fs-portfolio-app-frontend/src/Components/ChatViewItem.js
+++ fs-portfolio-app-frontend/src/Components/ChatViewItem.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { fetchUsersData } from '../API/auth';
 
 const ChatViewItem = ({message, id}) => {
+    const [singleUserName, setSingleUserName] = useState('')
+    const userID = { id }
+
     console.log(message)
     const { time, date } = message
+    
+
+    const singleUserByName = (userID, usersData) => {
+        console.log('login-page, singleUserByName, id arg value:', id)
+        console.log('login-page, singleUserByName, usersData arg value:', usersData.data)
+      
+        const user = usersData.data.find((userData) => userData.id === id)
+      
+        return user || null; // Return null if no match found
+      }
+      
+
+    const onPageLoad = async (id) => {
+        const usersData = await fetchUsersData()
+        console.log('++++++++++++++++++++++++++++++++++++',usersData)
+        const user = await singleUserByName(id, usersData)
+        console.log(user)
+        setSingleUserName(user)
+
+    }
+
+    useEffect(()=> {
+        onPageLoad()
+    },[])
+    
+
+
 
     return (
         <>
-            <Link to={`/chat/${id}`}>
-                <li className='message'>
-                    time: {time} date: {date} message: {message.message}
+                <li className='message-item'>
+                    <Link to={`/chat/${id}`}>
+                    {singleUserName.name}: {message.message} 
+                    </Link>
                 </li>
-            </Link>
+          
         </>
     );
 };
